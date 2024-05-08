bbb29576224d9522a288b8bc631873b929c62426
zejunma <zejunma9@gmail.com>
2024-05-08 05:30:58 -0400

    add Pose and Communication

---
+++ Pose/command.bat
@@ -0,0 +1,8 @@
+python demo2.py  ^
+mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py ^
+https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth ^
+configs/body_2d_keypoint/integral_regression/coco/ipr_res50_dsnt-8xb64-210e_coco-256x256.py ^
+https://download.openmmlab.com/mmpose/v1/body_2d_keypoint/integral_regression/coco/ipr_res50_dsnt-8xb64-210e_coco-256x256-441eedc0_20220913.pth ^
+configs/body_3d_keypoint/video_pose_lift/h36m/video-pose-lift_tcn-243frm-supv-cpn-ft_8xb128-200e_h36m.py ^
+https://download.openmmlab.com/mmpose/body3d/videopose/videopose_h36m_243frames_fullconv_supervised_cpn_ft-88f5abbb_20210527.pth  ^
+--input webcam 
